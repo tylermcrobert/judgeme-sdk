27c50243095ec561f2ef9873cea392962ffa608f
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-09-06 23:01:37 -0700

    remove lib

--- lib/lib/FetchReviewController/fetchAverage.d.ts
+++
@@ -1,5 +0,0 @@
-import { RatingAverages } from "../../types";
-export declare const fetchAverage: ({ external_id, shop_domain, }: {
-    external_id: string | number;
-    shop_domain: string;
-}) => Promise<RatingAverages | null>;
--- lib/lib/FetchReviewController/fetchReviews.d.ts
+++
@@ -1,7 +0,0 @@
-import { ReviewResponse } from "../../types";
-export declare const fetchReviews: (query: string) => Promise<ReviewResponse | null>;
-export declare const getDataFromHTML: (html: string) => {
-    lastPage: number | null;
-    nextPage: number | null;
-    reviews: any;
-};
